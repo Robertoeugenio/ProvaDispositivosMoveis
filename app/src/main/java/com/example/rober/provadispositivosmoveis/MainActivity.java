6d0f24e6b8a53ad36f256fd7cc41997d43a04490
robertoeugenio1978@gmail.com <robertoeugenio>
2018-09-10 20:36:46 -0300

    Initial commit

---
+++ app/src/main/java/com/example/rober/provadispositivosmoveis/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.rober.provadispositivosmoveis;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+
+     Edit text visor;
+     int operando1, operando2;
+     String operador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        visor = (EditText) findViewById(R.id.visor);
+
+
+    }
+
+    public void operacao (View botao){
+        switch (botao.getId()){
+            case.R.id.button1:
+            case.R.id.button2:
+            case.R.id.button3:
+            case.R.id.button4:
+            case.R.id.button5:
+            case.R.id.button6:
+            case.R.id.button7:
+            case.R.id.button8:
+            case.R.id.button9:
+            case.R.id.button10:
+            case.R.id.button11:
+            case.R.id.button12:
+            case.R.id.button13:
+            case.R.id.button14:
+            case.R.id.button15:
+            case.R.id.button16:
+            case R.id.button17;
+            case.R.id.button18;
+
+
+            visor.setTex
+
+
+        }
+    }
+}
